db758f2ef4b7d92dbcea8b7c7ad42f3418f31827
mecastor <mecastor87@gmail.com>
2024-08-25 14:33:22 +0100

    Added user update API endpoint to backend

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -67,4 +67,23 @@ export const getUser = async (req: Request, res: Response) => {
   }
 };
 
-export const updateUser = async (req: Request, res: Response) => {};
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const { email, name, password } = req.body;
+    user.email = email;
+    user.name = name;
+    user.password = password;
+    await user.save();
+
+    return res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};

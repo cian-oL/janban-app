4d605de00c743bbe963f0b4db05941b729aae77d
cian-oL <cianoleary2010@gmail.com>
2024-11-29 19:04:57 +0000

    Refactored Sign in functions to harmonise with reigster function file structure

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -1,7 +1,25 @@
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
+import { useSignInUser } from "@/api/authApiClient";
 import SignInForm from "@/forms/SignInForm";
+import { SignInFormData } from "@/types/userTypes";
+import { useAuthenticateUserSession } from "@/hooks/auth";
 
 const SignInPage = () => {
-  return <SignInForm />;
+  const { signInUser } = useSignInUser();
+  const navigate = useNavigate();
+  const { authenticateUserSession } = useAuthenticateUserSession();
+
+  const handleSignIn = (formData: SignInFormData) => {
+    signInUser(formData).then((data) => {
+      authenticateUserSession(data);
+      toast.success("Signed in");
+      navigate("/");
+    });
+  };
+
+  return <SignInForm onSave={handleSignIn} />;
 };
 
 export default SignInPage;

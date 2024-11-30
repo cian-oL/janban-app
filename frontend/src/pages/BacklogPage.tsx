2f711b0ea33bbbf195b82397c4b267d20f742d8e
mecastor <mecastor87@gmail.com>
2024-11-30 14:37:25 +0000

    construction notice on backlog page

--- frontend/src/pages/BacklogPage.tsx
+++ frontend/src/pages/BacklogPage.tsx
@@ -1,7 +1,14 @@
 import BacklogBoard from "@/components/BacklogBoard";
 
 const BacklogPage = () => {
-  return <BacklogBoard />;
+  return (
+    <>
+      <h1 className="text-xl font-bold bg-orange-500">
+        (Under Construction -- limited functionality)
+      </h1>
+      <BacklogBoard />
+    </>
+  );
 };
 
 export default BacklogPage;

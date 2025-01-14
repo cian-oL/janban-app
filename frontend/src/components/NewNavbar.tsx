12ae096626027eadcff3b1ffc0b3639a7235691c
cian-oL <cianoleary2010@gmail.com>
2025-01-14 08:11:01 +0000

    kanban nav change wip

--- frontend/src/components/NewNavbar.tsx
+++ frontend/src/components/NewNavbar.tsx
@@ -1,18 +1,13 @@
+import { Link } from "react-router-dom";
+
 import { RiKanbanView2 } from "react-icons/ri";
 import { MdOutlineTableRows } from "react-icons/md";
-
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuList,
-} from "@/components/ui/navigation-menu";
-import { Link } from "react-router-dom";
+import { FaChevronDown } from "react-icons/fa";
 
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
@@ -21,25 +16,22 @@ const NewNavbar = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="flex gap-2 bg-amber-300 rounded p-2 text-black font-bold hover:bg-white">
-        Go to...
+        <span>Go to...</span>
+        <FaChevronDown className="mt-1 pt-0.5" />
       </DropdownMenuTrigger>
-      <DropdownMenuContent
-        className={`text-white mr-2 "bg-indigo-800"
-        `}
-      >
-        <DropdownMenuLabel className="font-extrabold text-lg underline">
-          My Account
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <Link to="/my-profile">Profile Settings</Link>
+      <DropdownMenuContent className="ml-8 bg-amber-300">
+        <DropdownMenuItem className="hover:bg-amber-400">
+          <Link to="/kanban">
+            <RiKanbanView2 className="bg-amber-300 font-bold text-black hover:text-white" />
+            <span>Kanban</span>
+          </Link>
         </DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
-          <Link to="/kanban">Kanaban Board</Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <Link to="/backlog">Backlog</Link>
+          <Link to="/kanban/backlog">
+            <MdOutlineTableRows className="bg-amber-400 font-bold text-black hover:bg-white" />
+            <span>Backlog</span>
+          </Link>
         </DropdownMenuItem>
         <DropdownMenuSeparator />
       </DropdownMenuContent>

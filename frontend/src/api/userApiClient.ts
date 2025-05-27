cd7584d4e07770a2a88e505b711e9fcc057fcee2
mecastor <mecastor87@gmail.com>
2025-05-27 15:12:13 +0100

    More WIP

--- frontend/src/api/userApiClient.ts
+++ frontend/src/api/userApiClient.ts
@@ -14,7 +14,61 @@ export const createUser = async (
       withCredentials: true,
     })
     .then((response) => response.data)
-    .catch(() => {
+    .catch((err) => {
+      console.log(err);
       throw new Error("Error with create user request");
+    });
+};
+
+export const getAllUsers = async (
+  axiosInstance: AxiosInstance,
+  accessToken: string
+): Promise<User[]> => {
+  return axiosInstance
+    .get("/api/user/users", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+    .then((response) => response.data)
+    .catch((err) => {
+      console.log(err);
+      throw new Error("FError with fetching all users");
+    });
+};
+
+export const getUser = async (
+  axiosInstance: AxiosInstance,
+  accessToken: string
+): Promise<User> => {
+  return axiosInstance
+    .get("/api/user/profile", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+    .then((response) => response.data)
+    .catch((err) => {
+      console.log(err);
+      throw new Error("Error with fetching user");
+    });
+};
+
+export const updateUser = async (
+  formData: User & { confirmPassword: string },
+  axiosInstance: AxiosInstance,
+  accessToken: string
+): Promise<User> => {
+  return axiosInstance
+    .put("/api/user/profile", formData, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+    .then((response) => response.data)
+    .catch((err) => {
+      console.log(err);
+      throw new Error("Error with updating user");
     });
 };
--- frontend/src/hooks/useUser.ts
+++ frontend/src/hooks/useUser.ts
@@ -5,7 +5,12 @@ import { AccessTokenResponse } from "@/types/authTypes";
 import { toast } from "sonner";
 import { useAuthContext } from "@/contexts/AuthContext";
 import { useAxiosInstance } from "../api/authApiClient";
-import { createUser } from "@/api/userApiClient";
+import {
+  createUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+} from "@/api/userApiClient";
 
 const USERS_QUERY_KEY = "users";
 const USER_QUERY_KEY = "user";
@@ -24,7 +29,7 @@ export const useRegisterUser = () => {
 
       const optimisticUser: User = {
         ...formData,
-        racfid: "TBC",
+        racfid: `temp-${Date.now()}`,
       };
 
       queryClient.setQueryData<User[]>([USERS_QUERY_KEY], (old) => [
@@ -46,7 +51,7 @@ export const useRegisterUser = () => {
           ) || []
       );
 
-      queryClient.setQueryData<User>([USER_QUERY_KEY, newUser.racfid], newUser);
+      queryClient.setQueryData<User>([USER_QUERY_KEY], newUser);
     },
 
     onError: (err, _, context) => {
@@ -54,9 +59,8 @@ export const useRegisterUser = () => {
       queryClient.setQueryData([USERS_QUERY_KEY], context?.currentUsers);
     },
 
-    onSettled: (_, __, { racfid }) => {
+    onSettled: () => {
       queryClient.invalidateQueries({ queryKey: [USERS_QUERY_KEY] });
-      queryClient.invalidateQueries({ queryKey: [USER_QUERY_KEY, racfid] });
     },
   });
 };
@@ -65,87 +69,59 @@ export const useGetAllUsers = () => {
   const { accessToken } = useAuthContext();
   const axiosInstance = useAxiosInstance();
 
-  const getAllUsers = async (): Promise<User[]> => {
-    return axiosInstance
-      .get("/api/user/users", {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      })
-      .then((response) => response.data)
-      .catch((err) => {
-        console.log(err);
-        throw new Error("Failed to get all users");
-      });
-  };
-
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery("getAllUsers", getAllUsers);
-
-  if (error) {
-    console.log(error.toString());
-    toast.error("Failed to get all user data");
-  }
-
-  return { users, isLoading };
+  return useQuery({
+    queryKey: [USERS_QUERY_KEY],
+    queryFn: async () => getAllUsers(axiosInstance, accessToken),
+    staleTime: 5 * 60 * 1000, // 5 minutes
+    refetchOnWindowFocus: false,
+  });
 };
 
 export const useGetUser = () => {
   const { accessToken } = useAuthContext();
   const axiosInstance = useAxiosInstance();
 
-  const getUserRequest = async (): Promise<User> => {
-    return axiosInstance
-      .get("/api/user/profile", {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      })
-      .then((response) => response.data)
-      .catch((err) => {
-        console.log(err);
-        throw new Error("Failed to get user profile");
-      });
-  };
-
-  const { data: currentUser, isLoading } = useQuery("getUser", getUserRequest);
-
-  return { currentUser, isLoading };
+  return useQuery({
+    queryKey: [USER_QUERY_KEY],
+    queryFn: async () => getUser(axiosInstance, accessToken),
+    staleTime: 5 * 60 * 1000, // 5 minutes
+    refetchOnWindowFocus: false,
+  });
 };
 
 export const useUpdateUser = () => {
+  const queryClient = useQueryClient();
   const { accessToken } = useAuthContext();
   const axiosInstance = useAxiosInstance();
 
-  const updateUserRequest = async (formData: UserFormData): Promise<User> => {
-    return axiosInstance
-      .put("/api/user/profile", formData, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
-      })
-      .then((response) => response.data)
-      .catch(() => {
-        throw new Error("Failed to update user profile");
+  return useMutation({
+    mutationFn: async (formData: User & { confirmPassword: string }) =>
+      updateUser(formData, axiosInstance, accessToken),
+
+    onMutate: async (formData) => {
+      const queryId = formData.racfid;
+
+      await queryClient.cancelQueries({ queryKey: [USERS_QUERY_KEY] });
+      await queryClient.cancelQueries({ queryKey: [USER_QUERY_KEY] });
+
+      const currentUsers = queryClient.getQueryData([USERS_QUERY_KEY]);
+      const currentUser = queryClient.getQueryData([USER_QUERY_KEY]);
+
+      queryClient.setQueryData<User[]>([USERS_QUERY_KEY], (old) => {
+        old?.map((user) =>
+          user.racfid === queryId ? { ...user, ...formData } : old
+        );
       });
-  };
 
-  const {
-    mutateAsync: updateUser,
-    error,
-    reset,
-    isLoading,
-  } = useMutation(updateUserRequest);
+      return { currentUsers, currentUser };
+    },
 
-  if (error) {
-    console.log(error.toString());
-    toast.error("Error updating profile");
-    reset();
-  }
-
-  return { updateUser, isLoading };
+    onSuccess: (updatedUser, { racfid }) => {
+      queryClient.setQueryData<User[]>(USERS_QUERY_KEY, (old) =>
+        old?.map(
+          (user: User) => (user.racfid === racfid ? updatedUser : user) || []
+        )
+      );
+    },
+  });
 };

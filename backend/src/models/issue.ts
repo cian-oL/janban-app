08339529ba5855a50efa397c1b70e88bcfaef1dd
cian-oL <cianoleary2010@gmail.com>
2025-06-08 12:49:30 +0100

    Refactored issue model management in backend: added Enums to model with export for middleware validation

--- backend/src/models/issue.ts
+++ backend/src/models/issue.ts
@@ -1,14 +1,25 @@
 import mongoose from "mongoose";
 
+export const issueCategoryEnum: string[] = ["Story", "Bug", "Task", "Spike"];
+export const issueColumnIdEnum: string[] = [
+  "blocked",
+  "playReady",
+  "inDevelopment",
+  "testReady",
+  "testInProgress",
+  "demoReady",
+  "complete",
+];
+
 const IssueSchema = new mongoose.Schema({
-  issueCategory: { type: String, required: true },
+  issueCategory: { type: String, required: true, enum: issueCategoryEnum },
   isBacklog: { type: Boolean, required: true },
   issueCode: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   description: { type: String, required: true },
   storyPoints: { type: Number },
   assignee: { type: String },
-  columnId: { type: String, required: true },
+  columnId: { type: String, required: true, enum: issueColumnIdEnum },
   createdAt: { type: Date, required: true },
   lastUpdated: { type: Date, required: true },
 });

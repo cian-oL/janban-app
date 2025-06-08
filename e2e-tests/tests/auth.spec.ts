89b4737d2c4efe24aa6b8c30067f4a8ea257b8b6
cian-oL <cianoleary2010@gmail.com>
2025-06-08 16:57:08 +0100

    Refactored e2e tests in reflection of updated UI

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -2,31 +2,8 @@ import { test, expect } from "@playwright/test";
 
 const UI_URL = "http://localhost:5173";
 
-test("should allow user to sign in", async ({ page }) => {
-  // get sign in button & expect correct heading
-  await page.goto(UI_URL);
-  await page.getByRole("button", { name: "Sign In" }).nth(0).click();
-  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
-
-  // fill fields & click login
-  await page.locator("[name=racfid]").fill("J900000");
-  await page.locator("[name=password]").fill("Password?123");
-  await page.getByRole("button", { name: "Sign In" }).nth(1).click();
-
-  // check assertion for successful sign in by UI change
-  await expect(page.getByText("Signed in")).toBeVisible();
-  await expect(page.getByText("Get Started on your Tasks!")).toBeVisible();
-  await expect(page.getByText("Kanban Menu")).toBeVisible();
-  await expect(
-    page.getByRole("button", { name: "Go to Kanban" })
-  ).toBeVisible();
-});
-
 test("should be able to register sucessfully", async ({ page }) => {
-  // generate random user racfid to avoid duplicate registration error
-  const testRacfid = `J${Math.floor(
-    Math.random() * 99996 + 900002
-  ).toString()}`;
+  const userNumber = Math.floor(Math.random() * 9999);
 
   // access the register page from the sign in page
   await page.goto(UI_URL);
@@ -35,19 +12,34 @@ test("should be able to register sucessfully", async ({ page }) => {
   await expect(page.getByRole("heading", { name: "Register" })).toBeVisible();
 
   // success on correct completion of form
-  await page.locator("[name=racfid]").fill(testRacfid);
-  await page.locator("[name=name]").fill("Dorian Nakamoto");
-  await page
-    .locator("[name=email]")
-    .fill(`${Math.floor(Math.random() * 9999)}@email.com`);
+  await page.locator("[name=email]").fill(`${userNumber}@email.com`);
+  await page.locator("[name=name]").fill(`User ${userNumber}`);
   await page.locator("[name=password]").fill("Password?123");
   await page.locator("[name=confirmPassword]").fill("Password?123");
   await page.getByRole("button", { name: "Register" }).click();
 
   // check assertion by appropriate UI change
-  await expect(page.getByText("User registered")).toBeVisible();
+  await expect(page.getByText("registered")).toBeVisible();
   await expect(page.getByText("Get Started on your Tasks!")).toBeVisible();
-  await expect(page.getByText("Kanban Menu")).toBeVisible();
+  await expect(
+    page.getByRole("button", { name: "Go to Kanban" })
+  ).toBeVisible();
+});
+
+test("should allow user to sign in", async ({ page }) => {
+  // get sign in button & expect correct heading
+  await page.goto(UI_URL);
+  await page.getByRole("button", { name: "Sign In" }).nth(0).click();
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+
+  // fill fields & click login
+  await page.locator("[name=racfid]").fill("J000001");
+  await page.locator("[name=password]").fill("Password?123");
+  await page.getByRole("button", { name: "Sign In" }).nth(1).click();
+
+  // check assertion for successful sign in by UI change
+  await expect(page.getByText("Signed in")).toBeVisible();
+  await expect(page.getByText("Get Started on your Tasks!")).toBeVisible();
   await expect(
     page.getByRole("button", { name: "Go to Kanban" })
   ).toBeVisible();
@@ -57,13 +49,13 @@ test("should allow user to sign out", async ({ page }) => {
   // sign in
   await page.goto(UI_URL);
   await page.getByRole("button", { name: "Sign In" }).nth(0).click();
-  await page.locator("[name=racfid]").fill("J900001");
+  await page.locator("[name=racfid]").fill("J000001");
   await page.locator("[name=password]").fill("Password?123");
   await page.getByRole("button", { name: "Sign In" }).nth(1).click();
   await expect(page.getByText("Signed in")).toBeVisible();
 
   // sign out
-  await page.getByText("User Out").click();
+  await page.locator('svg.lucide-user').click();
   await page.getByRole("button", { name: "Sign Out" }).click();
 
   // check assertion by appropriate UI change
